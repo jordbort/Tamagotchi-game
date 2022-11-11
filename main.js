53dffb71c964b80fd77177b5af07aa811f3b5cb3
jordbort <jordan.bortner@gmail.com>
2022-11-11 00:23:27 -0500

    Added timers for boredom, hunger, and sleepiness

--- main.js
+++ main.js
@@ -41,22 +41,123 @@ const petAge = document.querySelector(".pet-age")
 const petHunger = document.querySelector(".pet-hunger")
 const petSleepiness = document.querySelector(".pet-sleepiness")
 const petBoredom = document.querySelector(".pet-boredom")
+const playBtn = document.querySelector(".pet-play")
+const feedBtn = document.querySelector(".pet-feed")
+const sleepBtn = document.querySelector(".pet-sleep")
+
+// event listeners
+// sleepBtn.addEventListener("click", () => {
+//     console.log("zzz")
+//     game.clearInterval(gameTimer)
+// })
 
 // name your pet
 // const chosenName = prompt("What is your pet's name?")
 const chosenName = "Dude"
 
+
 // instantiating (?) the pet
+// boredom is the number
+// bored is the timer
+// getBored() starts the timer and controls `alive`
+// hunger is the number
+// hungry is the timer
+// getHungry() starts the timer and controls `alive`
+// sleepiness is the number
+// sleepy is the timer
+// getSleepy() starts the timer and controls `alive`
 const pet = {
     name: chosenName,
     age: 0,
     alive: true,
+    boredom: 1,
     hunger: 1,
     sleepiness: 1,
-    boredom: 1,
+    getBored() {
+        const bored = setInterval( () => {
+            if(this.boredom >= 10) {
+                this.alive = false
+                console.log("boredom:", this.boredom, "alive:", this.alive)
+                petBoredom.textContent = `Boredom: ${pet.boredom}`
+                clearInterval(bored)
+            }
+            else {
+                console.log("boredom:", this.boredom, "alive:", this.alive)
+                petBoredom.textContent = `Boredom: ${pet.boredom}`
+                this.boredom++
+            }
+        }, 500)
+    },
+    getHungry() {
+        const hungry = setInterval( () => {
+            if(this.hunger >= 10) {
+                this.alive = false
+                console.log("hunger:", this.hunger, "alive:", this.alive)
+                petHunger.textContent = `Hunger: ${pet.hunger}`
+                clearInterval(hungry)
+            }
+            else {
+                console.log("hunger:", this.hunger, "alive:", this.alive)
+                petHunger.textContent = `Hunger: ${pet.hunger}`
+                this.hunger++
+            }
+        }, 500)
+    },
+    pauseHunger() {
+        clearInterval(this.hunger)
+    },
+    getSleepy() {
+        const sleepy = setInterval( () => {
+            if(this.sleepiness >= 10) {
+                this.alive = false
+                console.log("sleepiness:", this.sleepiness, "alive:", this.alive)
+                petSleepiness.textContent = `Sleepiness: ${pet.sleepiness}`
+                clearInterval(sleepy)
+            }
+            else {
+                console.log("sleepiness:", this.sleepiness, "alive:", this.alive)
+                petSleepiness.textContent = `Sleepiness: ${pet.sleepiness}`
+                this.sleepiness++
+            }
+        }, 500)
+    },
 }
-petName.textContent = `Name: ${pet.name}`
+pet.getBored()
+pet.getHungry()
+pet.getSleepy()
+petName.textContent = `Name: ${pet.name} (Alive = ${pet.alive})`
 petAge.textContent = `Age: ${pet.age}`
-petHunger.textContent = `Hunger: ${pet.hunger}`
-petSleepiness.textContent = `Sleepiness: ${pet.sleepiness}`
-petBoredom.textContent = `Boredom: ${pet.boredom}`
+
+// functions
+const decrHunger = () => {
+    pet.hunger+=1
+}
+const decrSleepiness = () => {
+    pet.sleepiness+=1
+}
+const decrBoredom = () => {
+    pet.boredom+=1
+}
+const decrAge = () => {
+    pet.age+=1
+}
+
+petHunger
+
+
+
+// const game = {
+//     start() {
+//         const gameTimer = setInterval( () => {
+//             if(this.timer === 10) {
+//                 console.log("test", this.timer, "/ 10")
+//                 clearInterval(gameTimer)
+//             }
+//             else {
+//                 console.log("test", this.timer, "/ 10");
+//                 this.timer++
+//             }
+//         }, 100);
+//     },
+//     timer: 1,
+// }
